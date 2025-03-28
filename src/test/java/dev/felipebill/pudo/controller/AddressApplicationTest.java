717e3287fee2af44f5a564c063769153020c59ee
billfelipe <bill.felipeluiz@gmail.com>
2025-03-28 11:17:46 -0300

    AddressController testing

--- src/test/java/dev/felipebill/pudo/controller/AddressApplicationTest.java
+++ src/test/java/dev/felipebill/pudo/controller/AddressApplicationTest.java
@@ -15,17 +15,23 @@ import dev.felipebill.pudo.model.State;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AddressApplicationTest {
 
-	@Autowired
-	TestRestTemplate restTemplate;
-	
-	@Test
-	void shouldReturnNewlyRegisteredAddressDataWithId() {
-		var form = new RegisterNewAddressForm("streetname", 123, "ne", "88888-888", State.ACRE.toString(), Country.BRASIL.toString());
-		ResponseEntity response = restTemplate.postForEntity("/addresses", form, NewlyRegisteredAddressData.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-	}
-	
-	
-	
-	
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldReturnNewlyRegisteredAddressDataWithId() {
+        var form = new RegisterNewAddressForm("streetname", 123, "ne", "81070-001", State.ACRE.toString(), Country.BRASIL.toString());
+        ResponseEntity<NewlyRegisteredAddressData> response = restTemplate.postForEntity("/addresses", form, NewlyRegisteredAddressData.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().id()).isNotNull();
+        assertThat(response.getBody().street()).isEqualTo(form.street());
+        assertThat(response.getBody().number()).isEqualTo(form.number());
+        assertThat(response.getBody().neighborhood()).isEqualTo(form.neighborhood());
+        assertThat(response.getBody().postalCode()).isEqualTo(form.postalCode());
+        assertThat(response.getBody().state()).isEqualTo(form.state());
+        assertThat(response.getBody().country()).isEqualTo(form.country());
+    }
+    
+    
 }
