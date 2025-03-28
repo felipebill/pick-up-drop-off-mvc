6485979bf40599c1d4f6d2edeb25c4e3921d31ee
billfelipe <bill.felipeluiz@gmail.com>
2025-03-28 11:31:04 -0300

    end-to-end testing

--- src/main/java/dev/felipebill/pudo/controller/AddressController.java
+++ src/main/java/dev/felipebill/pudo/controller/AddressController.java
@@ -1,14 +1,18 @@
 package dev.felipebill.pudo.controller;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import dev.felipebill.pudo.model.Address;
 import dev.felipebill.pudo.repository.AddressRepository;
 
 @RestController
@@ -16,23 +20,33 @@ import dev.felipebill.pudo.repository.AddressRepository;
 public class AddressController {
 
 	private final AddressRepository addressRepository;
-	
+
 	public AddressController(final AddressRepository addressRepository) {
 		this.addressRepository = addressRepository;
 	}
-	
+
+	@GetMapping("/{id}")
+	ResponseEntity<NewlyRegisteredAddressData> findById(@PathVariable Long id) {
+		Optional<Address> addressOptional = this.addressRepository.findById(id);
+		if (addressOptional.isPresent()) {
+			var data = NewlyRegisteredAddressData.fromAddress(addressOptional.get());
+			return ResponseEntity.ok(data);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+
 	@PostMapping
-	ResponseEntity<NewlyRegisteredAddressData> registerNewAddress(
-			@RequestBody RegisterNewAddressForm form,
-			UriComponentsBuilder uriComponentsBuilder
-			) {
+	ResponseEntity<NewlyRegisteredAddressData> registerNewAddress(@RequestBody RegisterNewAddressForm form,
+			UriComponentsBuilder uriComponentsBuilder) {
 		var address = form.toAddress();
 		this.addressRepository.save(address);
 		var data = NewlyRegisteredAddressData.fromAddress(address);
-		
+
 		URI uri = uriComponentsBuilder.path("/addresses/{id}").buildAndExpand(data.id()).toUri();
-		
+
 		return ResponseEntity.created(uri).body(data);
 	}
-	
+
 }
--- src/test/java/dev/felipebill/pudo/controller/AddressApplicationTest.java
+++ src/test/java/dev/felipebill/pudo/controller/AddressApplicationTest.java
@@ -15,23 +15,67 @@ import dev.felipebill.pudo.model.State;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AddressApplicationTest {
 
-    @Autowired
-    TestRestTemplate restTemplate;
+	@Autowired
+	TestRestTemplate restTemplate;
 
-    @Test
-    void shouldReturnNewlyRegisteredAddressDataWithId() {
-        var form = new RegisterNewAddressForm("streetname", 123, "ne", "81070-001", State.ACRE.toString(), Country.BRASIL.toString());
-        ResponseEntity<NewlyRegisteredAddressData> response = restTemplate.postForEntity("/addresses", form, NewlyRegisteredAddressData.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().id()).isNotNull();
-        assertThat(response.getBody().street()).isEqualTo(form.street());
-        assertThat(response.getBody().number()).isEqualTo(form.number());
-        assertThat(response.getBody().neighborhood()).isEqualTo(form.neighborhood());
-        assertThat(response.getBody().postalCode()).isEqualTo(form.postalCode());
-        assertThat(response.getBody().state()).isEqualTo(form.state());
-        assertThat(response.getBody().country()).isEqualTo(form.country());
-    }
-    
-    
+	@Test
+	void shouldReturnNewlyRegisteredAddressDataWithId() {
+		var form = new RegisterNewAddressForm("streetname", 123, "ne", "81070-001", State.ACRE.toString(),
+				Country.BRASIL.toString());
+		ResponseEntity<NewlyRegisteredAddressData> response = restTemplate.postForEntity("/addresses", form,
+				NewlyRegisteredAddressData.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(response.getBody()).isNotNull();
+		assertThat(response.getBody().id()).isNotNull();
+		assertThat(response.getBody().street()).isEqualTo(form.street());
+		assertThat(response.getBody().number()).isEqualTo(form.number());
+		assertThat(response.getBody().neighborhood()).isEqualTo(form.neighborhood());
+		assertThat(response.getBody().postalCode()).isEqualTo(form.postalCode());
+		assertThat(response.getBody().state()).isEqualTo(form.state());
+		assertThat(response.getBody().country()).isEqualTo(form.country());
+
+		String locationHeader = response.getHeaders().getLocation().toString();
+		assertThat(locationHeader).isNotNull();
+		assertThat(locationHeader).contains("/addresses/" + response.getBody().id());
+		Long id = response.getBody().id();
+		ResponseEntity<NewlyRegisteredAddressData> getAddressResponse = restTemplate.getForEntity(locationHeader,
+				NewlyRegisteredAddressData.class);
+
+		assertThat(getAddressResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(getAddressResponse.getBody()).isNotNull();
+		assertThat(getAddressResponse.getBody().id()).isEqualTo(id);
+		assertThat(getAddressResponse.getBody().street()).isEqualTo(form.street());
+		assertThat(getAddressResponse.getBody().number()).isEqualTo(form.number());
+		assertThat(getAddressResponse.getBody().neighborhood()).isEqualTo(form.neighborhood());
+		assertThat(getAddressResponse.getBody().postalCode()).isEqualTo(form.postalCode());
+		assertThat(getAddressResponse.getBody().state()).isEqualTo(form.state());
+		assertThat(getAddressResponse.getBody().country()).isEqualTo(form.country());
+	}
+
+	@Test
+	void shouldIncrementIdAfterRegisteringTwice() {
+		var form = new RegisterNewAddressForm("streetname", 123, "ne", "81070-001", State.ACRE.toString(),
+				Country.BRASIL.toString());
+		ResponseEntity<NewlyRegisteredAddressData> response1 = restTemplate.postForEntity("/addresses", form,
+				NewlyRegisteredAddressData.class);
+		assertThat(response1.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(response1.getBody()).isNotNull();
+		assertThat(response1.getBody().id()).isNotNull();
+		String locationHeader1 = response1.getHeaders().getLocation().toString();
+		assertThat(locationHeader1).isNotNull();
+		assertThat(locationHeader1).contains("/addresses/" + response1.getBody().id());
+
+		ResponseEntity<NewlyRegisteredAddressData> response2 = restTemplate.postForEntity("/addresses", form,
+				NewlyRegisteredAddressData.class);
+		assertThat(response2.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(response2.getBody()).isNotNull();
+		assertThat(response2.getBody().id()).isNotNull();
+		String locationHeader2 = response2.getHeaders().getLocation().toString();
+		assertThat(locationHeader2).isNotNull();
+		assertThat(locationHeader2).contains("/addresses/" + response2.getBody().id());
+
+		assertThat(response1.getBody().id()).isNotEqualTo(response2.getBody().id());
+
+	}
 }
