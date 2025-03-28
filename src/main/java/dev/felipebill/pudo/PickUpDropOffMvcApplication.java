fa429d6346fbcaf93a0f1d0606586f5ec56c757e
billfelipe <bill.felipeluiz@gmail.com>
2025-03-28 09:21:35 -0300

    initial commit

---
+++ src/main/java/dev/felipebill/pudo/PickUpDropOffMvcApplication.java
@@ -0,0 +1,13 @@
+package dev.felipebill.pudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickUpDropOffMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickUpDropOffMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/felipebill/pudo/PickUpDropOffMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.felipebill.pudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickUpDropOffMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
