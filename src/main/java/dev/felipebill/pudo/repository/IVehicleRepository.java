12f7b0e62eeb6ca9c116c417c05c4a31f42012a2
billfelipe <bill.felipeluiz@gmail.com>
2025-03-31 11:32:43 -0300

    findById, findAll and findByPlateNumber

--- src/main/java/dev/felipebill/pudo/repository/IVehicleRepository.java
+++ src/main/java/dev/felipebill/pudo/repository/IVehicleRepository.java
@@ -1,13 +1,18 @@
 package dev.felipebill.pudo.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import dev.felipebill.pudo.model.Vehicle;
 
 @Repository
-public interface IVehicleRepository extends CrudRepository<Vehicle, Long> {
+public interface IVehicleRepository extends CrudRepository<Vehicle, Long>, PagingAndSortingRepository<Vehicle, Long> {
 
-	
-	
+	@Query("SELECT v FROM Vehicle v WHERE v.plate.value = :plateNumber")
+	Optional<Vehicle> findByPlateNumber(String plateNumber);
+
 }
--- src/main/java/dev/felipebill/pudo/service/VehicleService.java
+++ src/main/java/dev/felipebill/pudo/service/VehicleService.java
@@ -1,5 +1,7 @@
 package dev.felipebill.pudo.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import dev.felipebill.pudo.model.Vehicle;
@@ -10,20 +12,27 @@ import jakarta.persistence.EntityNotFoundException;
 public class VehicleService {
 
 	private IVehicleRepository vehicleRepository;
-	
+
 	public VehicleService(final IVehicleRepository vehicleRepository) {
 		this.vehicleRepository = vehicleRepository;
 	}
-	
+
 	public void registerNewVehicle(Vehicle vehicle) {
 		this.vehicleRepository.save(vehicle);
 	}
 
 	public Vehicle findById(Long id) {
-		return this.vehicleRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Vehicle not found for id: " + id));
+		return this.vehicleRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("Vehicle not found for id: " + id));
 	}
-	
-	
-	
-	
+
+	public Page<Vehicle> findAll(Pageable pageable) {
+		return this.vehicleRepository.findAll(pageable);
+	}
+
+	public Vehicle findByPlateNumber(String plateNumber) {
+		return this.vehicleRepository.findByPlateNumber(plateNumber)
+				.orElseThrow(() -> new EntityNotFoundException("Vehicle not found for plate number: " + plateNumber));
+	}
+
 }
