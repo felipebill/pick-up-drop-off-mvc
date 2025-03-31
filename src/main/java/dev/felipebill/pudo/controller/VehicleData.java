f581ccd6656bbab3868925dda56b2e5260cece7f
billfelipe <bill.felipeluiz@gmail.com>
2025-03-31 10:59:52 -0300

    Fix

--- src/main/java/dev/felipebill/pudo/controller/VehicleData.java
+++ src/main/java/dev/felipebill/pudo/controller/VehicleData.java
@@ -5,7 +5,7 @@ import dev.felipebill.pudo.model.Vehicle;
 public record VehicleData(Long id, String driversName, String plateNumber, Integer capacity) {
 
 	public static VehicleData fromVehicle(final Vehicle vehicle) {
-		return new VehicleData(vehicle.getId(), vehicle.getDriversName(), vehicle.getPlateNumber().getValue(),
+		return new VehicleData(vehicle.getId(), vehicle.getDriversName(), vehicle.getPlate().getValue(),
 				vehicle.getCapacity());
 	}
 
