a8f983aec55008b257da311f157323d5c6a6246b
billfelipe <bill.felipeluiz@gmail.com>
2025-03-31 10:29:42 -0300

    Vehicle entity mapping

--- src/main/java/dev/felipebill/pudo/model/Vehicle.java
+++ src/main/java/dev/felipebill/pudo/model/Vehicle.java
@@ -2,6 +2,13 @@ package dev.felipebill.pudo.model;
 
 import java.util.Collection;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +20,25 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name = "TB_VEHICLE")
 public class Vehicle {
 
+	@Id
+	@Column(name = "ID_VEHICLE")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "DS_PLATE")
 	private String plateNumber;
+	
+	@Column(name = "DS_DRIVERS_NAME")
 	private String driversName;
+	
+	@Column(name = "NR_CAPACITY")
 	private Integer capacity;
+	
+	@Transient
 	private Collection<Delivery> deliveries;
 	
 }
